3c8bb3dc85102bc22d1b704e5a15f3087a2d7c84
htirawi <hmtirawi@gmail.com>
2018-07-25 20:40:46 +0300

    commonCharacter problem

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -12,5 +12,16 @@
 
 //Works for any number of input strings:
 var commonCharacters = function(string1, string2) {
-  
+  var newStr = "";
+
+	for(var i = 0 ; i < string1.length;i++){
+		for(var j = 0; j < string2.length;j++){
+			if(string1[i] === strings[j]){
+				newStr+=string1[i]
+			}
+		}
+	}
+
+	return newStr;
+
 };
