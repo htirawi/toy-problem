a7ed2efe3430315cb96c68a000ceb64984498452
htirawi <hmtirawi@gmail.com>
2018-07-29 23:41:57 +0300

    filterPalindrome toyproblem solution

--- filterPalindromes/filterPalindromes.js
+++ filterPalindromes/filterPalindromes.js
@@ -10,6 +10,26 @@
 
 
 function filterPalindromes (words) {
-  // Write your code here, and
-  // return your final answer.
+ // Write your code here, and
+ // return your final answer.
+ 
+ var arr = [];
+ var flag = true;
+ for(var i = 0 ; i < words.length; i++){
+ 	var wordLength = words[i].length-1;
+ 	for(var j = 0 ; j < words[i].length; j++){
+ 		if(words[i][j].toLowerCase() === words[i][wordLength].toLowerCase()){
+ 			flag = true;
+ 		}
+ 		else{
+ 			flag = false;
+ 		}
+ 		wordLength--;
+ 	}
+ 	if(flag === true){
+ 		arr.push(words[i])
+ 	}
+ }
+ 
+ return arr;
 }
