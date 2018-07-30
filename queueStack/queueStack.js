a1a7da069394c630a553af2b71093322a0bcad35
htirawi <hmtirawi@gmail.com>
2018-07-30 18:33:13 +0300

    queueStack toy problem solution

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -6,16 +6,22 @@
 var Stack = function() {
 	var storage = [];
 
-	this.push = function(){
+	this.push = function(value){
+		storage.push(value);
 
 	};
 
 	this.pop = function(){
-
+		if(storage.length<0){
+			return null
+		}
+		else{
+			return storage.pop();
+		}
 	};
 
 	this.size = function(){
-
+		return storage.length;
 	};
 };
 
@@ -25,14 +31,24 @@ var Queue = function() {
 	var outbox = new Stack();
 
 	this.enqueue = function(){
+		return inbox.push(value);
 
 	};
 
 	this.dequeue = function(){
 		
+		if(outbox.size() === 0 ){
+			while(inbox.size !== 0){
+				outbox.push(inbox.pop())
+			}
+		}
+		return outbox.pop()
 	};
 
 	this.size = function(){
+
+		var total = inbox.size() + outbox.size();
+		return total;
 
 	};
 };
