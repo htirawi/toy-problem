48b3444c1990a688f19cc631d48a3f294d521b17
htirawi <hmtirawi@gmail.com>
2018-08-06 11:47:20 +0300

    primeTester toyproblem solution

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -10,4 +10,15 @@
 function primeTester (n) {
   // Write your code here, and
   // return your final answer.
+
+  if(n<=1){
+  	return false;
+  }
+
+  for (var i = 2; i < Math.sqrt(n+1); i++) {
+  	if (n % i === 0) {
+  		return false;
+  	}
+  }
+  return true;
 }
