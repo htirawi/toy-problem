84fe6748d02d477c7d54742c2a532fffad77b2c1
htirawi <hmtirawi@gmail.com>
2018-08-09 10:48:54 +0300

    nestedWordCount toyproblem solution

--- wordsWthinWords/wordsWthinWords.js
+++ wordsWthinWords/wordsWthinWords.js
@@ -6,3 +6,28 @@
 //[ "gray", "grays", "ray", "rays", "strays" ]	
 //output:"grays"
 
+
+
+function nestedWordCount (wordList) {
+	var word = "";
+	var count = 0;
+	var max = 0 ;
+
+	for(var i = 0 ; i< wordList.length;i++){
+		for(var j = 0 ; j< wordList.length;j++ ){
+			if(wordList[i].includes(wordList[j])){
+				count++;
+
+			}
+
+		}
+
+		if(count>max){
+			max = count;
+			word = wordList[i];
+		}
+		count = 0;
+	}
+	return word;
+
+}
