0c917382526c07bbd6603f2cd5c67589633f286d
htirawi <hmtirawi@gmail.com>
2018-08-13 01:31:46 +0300

    longestRun toyproblem solution

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -20,9 +20,23 @@ If there are two runs of equal length, return the first one. Return [0,0] for no
 
 
 function longestRun (string) {
-
-
-
-
-
+	if (string.length === 0)
+	{
+		return [0,0];
+	}
+	var index = 0, high = 0, count = 0, start = 0;
+	for (var i = 0; i < string.length; i++) {
+		var char = string[i];
+		if (char !== string[i-1]) {
+			count = 1;
+			start = i;
+		} else {
+			count++;
+		}
+		if (count > high) {
+			high = count;
+			index = start;
+		}
+	}
+	return [index, index + high - 1];
 }
