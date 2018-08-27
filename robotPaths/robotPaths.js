e0d0005074330be3cdea62461b08a1e0837eb81e
htirawi <hmtirawi@gmail.com>
2018-08-27 11:02:55 +0300

    robotPaths toyproblem solution

--- robotPaths/robotPaths.js
+++ robotPaths/robotPaths.js
@@ -19,4 +19,48 @@ n:
 function robotPaths (n) {
   // Write your code here, and
   // return your final answer.
+  
+  var board = makeBoard(n);
+  var sol = 0;
+  var recurse = function(r,c) {
+  	board.togglePiece(r,c);
+
+  	if(r === n-1 && c === n-1){
+  		sol++;
+  		board.togglePiece(r,c);
+  		return;
+  	}
+  	if(r-1 >= 0 && !board.hasBeenVisited(r-1,c)){
+  		recurse(r-1,c);
+  	}
+  	if(c+1 < n && !board.hasBeenVisited(r,c+1)){
+  		recurse(r,c+1);
+  	}
+  	if(r+1 < n && !board.hasBeenVisited(r+1,c)){
+  		recurse(r+1,c);
+  	}
+  	if(c-1 >=0 && !board.hasBeenVisited(r,c-1)){
+  		recurse(r,c-1);
+  	}
+  	board.togglePiece(r,c);
+  };
+  recurse(0,0);
+  return sol;
+}
+
+function makeBoard(n) {
+	var board = [];
+	for (var i = 0; i < n; i++) {
+		board.push([]);
+		for (var j = 0; j < n; j++) {
+			board[i].push(false);
+		}
+	}
+	board.togglePiece = function(i, j) {
+		this[i][j] = !this[i][j];
+	}
+	board.hasBeenVisited = function(i, j) {
+		return !!this[i][j];
+	}
+	return board;
 }
