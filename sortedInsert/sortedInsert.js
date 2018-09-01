f5dd179cbed87f6a263fb47dc437e6045baf475d
htirawi <hmtirawi@gmail.com>
2018-09-01 10:44:22 +0300

    trying to solve sortedInsert

--- sortedInsert/sortedInsert.js
+++ sortedInsert/sortedInsert.js
@@ -7,8 +7,36 @@ The stack will have the methods, isEmpty, peek, push, and pop.
 You can also use count to view how many elements are in the stack.
 */
 
+var Stack = function (initialValue) {
+	this.store = {};
+	this.count = 0;
+	
+	if (initialValue !== undefined) {
+		this.push(initialValue);
+	}
+};
+
+Stack.prototype.isEmpty = function() {
+  return !this.count; // !0 = true
+};
+
+Stack.prototype.peek = function() {
+  return this.store[this.count]; // returns the value at this.count key
+};
+
+Stack.prototype.push = function(val) {
+  this.store[this.count++] = val; // defining value at incremented this.count key
+};
+
+Stack.prototype.pop = function() {
+	if (!this.isEmpty()) {
+		var tmp = this.store[this.count];
+		delete this.store[this.count--];
+		return tmp;
+	}
+};
+
+
 function sortedInsert(stack, element) {
-  //
-  // TODO
-  //
-} 
+	
+}
